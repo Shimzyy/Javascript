216c0f564738a914a6a771f889c3120bfefcf3e8
Shimzy <shims.ahua@gmail.com>
2022-07-18 09:48:30 +0100

    idk lmao

---
+++ objects and methods/index.js
@@ -0,0 +1,12 @@
+let ex1 = document.getElementById("Ex")
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+  document.getElementById("Ex").innerHTML = "vkoepeeb"
