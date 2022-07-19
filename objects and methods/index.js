8719cfa106ca79812b55baeacc3d77758bfcde70
Shimzy <shims.ahua@gmail.com>
2022-07-19 13:45:21 +0100

    finally got the external js to work

--- objects and methods/index.js
+++ objects and methods/index.js
@@ -1,15 +1,18 @@
 // let ex1 = document.getElementById("Ex")
 
 const person = {
-    firstName: "John",
-    lastName: "Doe",
-    id: 5566,
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    }
-  };
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function() {
+    return "Hi, my name is" + " \"Johnnee\" " + " " + this.lastName;
+  }
+};
 
-document.getElementById("Ex").textContent = "vkoepeeb"
+// Display data from the object:
+document.getElementById("demo").innerHTML = person.fullName();
+
+let ex1 = document.getElementById("example").textContent = 4;
 console.log("hi")
 alert("hey babe")
 console.error("this is an error");
